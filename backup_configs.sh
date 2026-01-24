4654aeb25e54575e59ed7b36c19e9d2a4b678fef
Ashish <232292705+ashd90@users.noreply.github.com>
2026-01-24 08:37:01 +0530

    Automated backup: 2026-01-24 08:37:01

--- backup_configs.sh
+++ backup_configs.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
 # --- 1. Variables (Clean & Maintainable) ---
-BACKUP_DIR="$HOME/devops/automation-suite"
+BACKUP_DIR="$HOME/repoHive/devbox/automation-suite/"
 CONFIG_SRC="$HOME/.config"
 TIMESTAMP=$(date +"%Y-%m-%d %H:%M:%S")
 LOG_FILE="$BACKUP_DIR/backup.log"
 
 # --- 2. Define target apps to track ---
-TARGETS=("hypr" "mpv" "waybar" "mpd")
+TARGETS=("ghostty")
 
 echo "[$TIMESTAMP] Starting Config Backup..." | tee -a "$LOG_FILE"
 
 # --- 3. Sync Files (Using 'cp' or 'rsync') ---
 for item in "${TARGETS[@]}"; do
-    if [ -d "$CONFIG_SRC/$item" ]; then
-        mkdir -p "$BACKUP_DIR/$item"
-        cp -r "$CONFIG_SRC/$item/"* "$BACKUP_DIR/$item/"
-        echo "Successfully synced: $item" | tee -a "$LOG_FILE"
-    else
-        echo "Warning: $item config not found!" | tee -a "$LOG_FILE"
-    fi
+  if [ -d "$CONFIG_SRC/$item" ]; then
+    mkdir -p "$BACKUP_DIR/$item"
+    cp -r "$CONFIG_SRC/$item/"* "$BACKUP_DIR/$item/"
+    echo "Successfully synced: $item" | tee -a "$LOG_FILE"
+  else
+    echo "Warning: $item config not found!" | tee -a "$LOG_FILE"
+  fi
 done
 
 # --- 4. GitOps Automation ---
@@ -27,12 +27,12 @@ cd "$BACKUP_DIR" || exit
 
 # Check if there are actual changes before committing
 if [[ -n $(git status --porcelain) ]]; then
-    git add .
-    git commit -m "Automated backup: $TIMESTAMP"
-    git push origin main
-    echo "Changes committed to Git." | tee -a "$LOG_FILE"
+  git add .
+  git commit -m "Automated backup: $TIMESTAMP"
+  git push origin main
+  echo "Changes committed to Git." | tee -a "$LOG_FILE"
 else
-    echo "No changes detected. Skipping commit." | tee -a "$LOG_FILE"
+  echo "No changes detected. Skipping commit." | tee -a "$LOG_FILE"
 fi
 
-echo "-------------------------------------" >> "$LOG_FILE"
+echo "-------------------------------------" >>"$LOG_FILE"
